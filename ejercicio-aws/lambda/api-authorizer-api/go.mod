5b011a520a353d5169b127c695709e494afd04c7
Juan Ignacio Piccardi <juan.piccardi@uala.com.ar>
2022-11-16 16:41:29 -0300

    autenticacion

---
+++ ejercicio-aws/lambda/api-authorizer-api/go.mod
@@ -0,0 +1,5 @@
+module github.com/jipiccardi/ejercicios-onboarding-uala/ejercicio-aws/lambda/api-authorizer-api
+
+go 1.19
+
+require github.com/aws/aws-lambda-go v1.34.1
---
+++ ejercicio-aws/lambda/api-authorizer-api/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// Help function to generate an IAM policy
+func generatePolicy(principalId, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
+	authResponse := events.APIGatewayCustomAuthorizerResponse{PrincipalID: principalId}
+
+	if effect != "" && resource != "" {
+		authResponse.PolicyDocument = events.APIGatewayCustomAuthorizerPolicy{
+			Version: "2012-10-17",
+			Statement: []events.IAMPolicyStatement{
+				{
+					Action:   []string{"execute-api:Invoke"},
+					Effect:   effect,
+					Resource: []string{resource},
+				},
+			},
+		}
+	}
+
+	return authResponse
+}
+
+func handleRequest(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
+	token := event.AuthorizationToken
+	switch strings.ToLower(token) {
+	case "allow":
+		return generatePolicy("user", "Allow", event.MethodArn), nil
+	case "deny":
+		return generatePolicy("user", "Deny", event.MethodArn), nil
+	case "unauthorized":
+		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized") // Return a 401 Unauthorized response
+	default:
+		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("error: Invalid token")
+	}
+}
+
+func main() {
+	lambda.Start(handleRequest)
+}
